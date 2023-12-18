0b220ef06c73a5e6e5777577397f4149ef7a243f
ZellosMet <zellossmet@gmail.com>
2023-12-18 23:20:10 +0900

    SELECT

---
+++ SELECT.sql
@@ -0,0 +1,14 @@
+SELECT 
+	last_name AS 'Фамилия', 
+	first_name AS 'Имя', 
+	middle_name AS 'Отчество', 
+	--DATEDIFF(yyyy, Students.birth_date, GETDATE()) AS 'Возраст',
+	--GETDATE() - CONVERT(datetime, Students.birth_date) AS 'Возраст',
+	group_name AS 'Группа'
+	--direction_name AS 'Направление'
+FROM Students, Groups, Directions
+WHERE Students.[group] = Groups.group_id
+--AND Groups.group_name = 'PD_212';
+AND Groups.direction = Directions.direction_id
+--AND Directions.direction_name = 'Java Development'
+AND Directions.direction_name = 'Разработка программного обеспечения'
