ce9ec2bf7b8af89f3e509e6305b57a1293d2c240
ZellosMet <zellossmet@gmail.com>
2023-12-21 20:29:09 +0900

    Schedule DONE

--- UPDATE Groups for Schedule.sql
+++ UPDATE Groups for Schedule.sql
@@ -1,4 +1,4 @@
-USE PD_212
+п»їUSE PD_212
 
 UPDATE Schedule SET [group] = (SELECT group_id FROM Groups WHERE group_name LIKE '%Java_326%') 
-				WHERE discipline = (SELECT discipline_id FROM Disciplines WHERE discipline_name LIKE 'Основы программирования на языке Java');
+				WHERE discipline = (SELECT discipline_id FROM Disciplines WHERE discipline_name LIKE 'РћСЃРЅРѕРІС‹ РїСЂРѕРіСЂР°РјРјРёСЂРѕРІР°РЅРёСЏ РЅР° СЏР·С‹РєРµ Java');
