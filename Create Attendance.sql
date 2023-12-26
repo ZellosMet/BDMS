704d4e1a7ca29ef936f75586c08558d1c2363fb9
ZellosMet <zellossmet@gmail.com>
2023-12-26 21:32:55 +0900

    Fill Grades & Attendances

--- Create Attendance.sql
+++ Create Attendance.sql
@@ -1,10 +1,10 @@
 ﻿USE PD_212
 
 DECLARE @student			INT			= 3
-DECLARE @lesson				BIGINT		= 335814
+DECLARE @lesson				BIGINT		= 1162
 DECLARE @present			TINYINT		= (SELECT ROUND(RAND()*(1-0)+0, 0))
 
-WHILE(@lesson<=338271)
+WHILE(@lesson<=1257)
 BEGIN
 	WHILE(@student<=23)
 	BEGIN
@@ -17,10 +17,26 @@ BEGIN
 	SET @present = (SELECT ROUND(RAND()*(1-0)+0, 0))
 END
 
-SET @lesson	= 202
+SET @lesson	= 1494
 SET @student = 3;
 
-WHILE(@lesson<=297)
+WHILE(@lesson<=1517)
+BEGIN
+	WHILE(@student<=23)
+	BEGIN
+			INSERT INTO Attendance(stud_id, lessons_id, present)
+			VALUES (@student, @lesson, @present);
+			SET @student += 1;
+	END
+	SET @student = 3;
+	SET @lesson	+= 2
+	SET @present = (SELECT ROUND(RAND()*(1-0)+0, 0))
+END
+
+SET @lesson	= 2598
+SET @student = 3;
+
+WHILE(@lesson<=2769)
 BEGIN
 	WHILE(@student<=23)
 	BEGIN
@@ -35,10 +51,12 @@ END
 
 SELECT 
 		[Студент] = FORMATMESSAGE('%s %s %s', last_name, first_name, middle_name),
-		[Дисциплина] = Attendance.lessons_id,
+		[Дисциплина] = Disciplines.discipline_name,
+		[Дата занятия] = Schedule.[date],
 		[Посещение] = IIF(Attendance.present = 1, 'Присутствовал', 'Отсутствовал')
-FROM Attendance, Students, Schedule
+FROM Attendance, Students, Schedule, Disciplines
 WHERE 
 	Attendance.stud_id = Students.stud_id
 AND Attendance.lessons_id = Schedule.lesson_id
+AND Schedule.discipline = Disciplines.discipline_id
 ORDER BY [Дисциплина]
--- Create Grades.sql
+++ Create Grades.sql
@@ -1,36 +1,77 @@
 ﻿USE PD_212
 
 DECLARE @student			INT			= (SELECT ROUND(RAND()*(23-4)+4, 0))
-DECLARE @lesson				BIGINT		= (SELECT ROUND(RAND()*(338271-338202)+338202, 0))
+DECLARE @lesson				BIGINT		= (SELECT ROUND(RAND()*(1257-1162)+1162, 0))
 DECLARE @grade_1			TINYINT		= (SELECT ROUND(RAND()*(12-1)+1, 0))
 DECLARE @grade_2			TINYINT		= (SELECT ROUND(RAND()*(12-1)+1, 0))
 DECLARE @counter			TINYINT		= 0
 
-WHILE(@counter<11)
-BEGIN
-	
+WHILE(@counter < 48)
+BEGIN	
 	IF	(SELECT COUNT(*) FROM Grades WHERE stud_id = @student AND lesson_id = @lesson) = 0
 	BEGIN
 		INSERT INTO Grades(stud_id, lesson_id, grade_1, grade_2)
 		VALUES (@student, @lesson, @grade_1, @grade_2);
 	END
 	SET @student = (SELECT ROUND(RAND()*(23-3)+3, 0))
-	SET @lesson	= (SELECT ROUND(RAND()*(338271-338202)+338202, 0))
+	SET @lesson	= (SELECT ROUND(RAND()*(1257-1162)+1162, 0))
 	SET @grade_1 = (SELECT ROUND(RAND()*(12-1)+1, 0))
 	SET @grade_2 = (SELECT ROUND(RAND()*(12-1)+1, 0))
 
-	SET @counter = @counter + 1;
+	SET @counter += 1;
 	PRINT @lesson
 	PRINT @student
 END
---SELECT ROUND(RAND()*(338271-200)+200, 0)
+
+SET @counter = 0;
+SET @lesson	= (SELECT ROUND(RAND()*(1517-1494)+1494, 0))
+
+WHILE(@counter<11)
+BEGIN	
+	IF	(SELECT COUNT(*) FROM Grades WHERE stud_id = @student AND lesson_id = @lesson) = 0
+	BEGIN
+		INSERT INTO Grades(stud_id, lesson_id, grade_1, grade_2)
+		VALUES (@student, @lesson, @grade_1, @grade_2);
+	END
+	SET @student = (SELECT ROUND(RAND()*(23-3)+3, 0))
+	SET @lesson	= (SELECT ROUND(RAND()*(1517-1494)+1494, 0))
+	SET @grade_1 = (SELECT ROUND(RAND()*(12-1)+1, 0))
+	SET @grade_2 = (SELECT ROUND(RAND()*(12-1)+1, 0))
+
+	SET @counter += 1;
+	PRINT @lesson
+	PRINT @student
+END
+
+SET @counter = 0;
+SET @lesson	= (SELECT ROUND(RAND()*(2769-2598)+2598, 0))
+
+WHILE(@counter<82)
+BEGIN	
+	IF	(SELECT COUNT(*) FROM Grades WHERE stud_id = @student AND lesson_id = @lesson) = 0
+	BEGIN
+		INSERT INTO Grades(stud_id, lesson_id, grade_1, grade_2)
+		VALUES (@student, @lesson, @grade_1, @grade_2);
+	END
+	SET @student = (SELECT ROUND(RAND()*(23-3)+3, 0))
+	SET @lesson	= (SELECT ROUND(RAND()*(2769-2598)+2598, 0))
+	SET @grade_1 = (SELECT ROUND(RAND()*(12-1)+1, 0))
+	SET @grade_2 = (SELECT ROUND(RAND()*(12-1)+1, 0))
+
+	SET @counter += 1;
+	PRINT @lesson
+	PRINT @student
+END
+
 SELECT 
 		[Студент] = FORMATMESSAGE('%s %s %s', last_name, first_name, middle_name),
-		[Дисциплина] = Grades.lesson_id,
+		[Дисциплина] = Disciplines.discipline_name,
+		[Дата занятия] = Schedule.[date],
 		[Оценка 1] = Grades.grade_1,
 		[Оценка 2] = Grades.grade_2
-FROM Grades, Students, Schedule
+FROM Grades, Students, Schedule, Disciplines
 WHERE 
 	Grades.stud_id = Students.stud_id
 AND Grades.lesson_id = Schedule.lesson_id
+AND Schedule.discipline = Disciplines.discipline_id
 ORDER BY [Дисциплина]
